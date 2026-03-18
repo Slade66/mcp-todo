04c6c04d276fc07cde03d983aad30923a221e41d
slade <slade66@tutamail.com>
2026-03-18 17:50:30 +0800

    SQLite 操作的测试

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
-	"github.com/Slade66/mcp-todo/internal/todo/repo/sqlite"
+	"context"
+	"fmt"
+
+	"github.com/Slade66/mcp-todo/internal/todo/biz"
+	"github.com/Slade66/mcp-todo/internal/todo/data/sqlite"
 )
 
 func main() {
@@ -10,4 +14,36 @@ func main() {
 		panic(err)
 	}
 	db.Migrate()
+
+	ctx := context.Background()
+
+	db.Create(ctx, &biz.CreateTodoInput{
+		Title:       "吃噜噜肉",
+		Description: "我都好久没吃噜噜肉了，今天一定要把噜噜吃了！",
+		Completed:   false,
+	})
+
+	db.Create(ctx, &biz.CreateTodoInput{
+		Title:       "回家拿礼物",
+		Description: "噜噜今天心情不好，得拿点小礼物收买她！",
+		Completed:   false,
+	})
+
+	rows, _ := db.List(ctx, nil)
+	for _, row := range rows {
+		fmt.Println(row)
+	}
+
+	desc := "明天是噜噜的生日，今晚过去陪她？"
+
+	db.Update(ctx, &biz.UpdateTodoInput{
+		ID:          biz.TodoID(2),
+		Description: &desc,
+	})
+
+	todo, _ := db.GetByID(ctx, biz.TodoID(2))
+	fmt.Println(todo)
+
+	db.Delete(ctx, biz.TodoID(2))
+
 }
